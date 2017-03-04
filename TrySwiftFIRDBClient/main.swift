b25d49abcaaece4bdb7730f902d554e3085158c2
Takayoshi Fujiki <takayoshi.fujiki@weblio.co.jp>
2017-03-04 12:15:24 +0900

    Initial Commit

---
+++ TrySwiftFIRDBClient/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TrySwiftFIRDBClient
+//
+//  Created by Takayoshi Fujiki on 2017/03/04.
+//  Copyright © 2017 Weblio, Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
